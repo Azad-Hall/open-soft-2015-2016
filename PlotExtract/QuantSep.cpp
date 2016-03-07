25089f4c381f688ca08be5072c7c140a9b17b0d3
satya-kgp <satyanand1994@gmail.com>
2016-03-07 19:06:12 +0530

    updated finalSep

--- PlotExtract/QuantSep.cpp
+++ PlotExtract/QuantSep.cpp
@@ -15,12 +15,43 @@
 using namespace cv;
 struct Color {
 	int r, g, b;
+	long long count;
 	Color(int r, int g, int b) : r(r), g(g), b(b) {}
 	Color(Vec3b c) : r(c[0]), g(c[1]), b(c[2]) {}
 	Color() : r(0), g(0), b(0) {}
+	//Color(Color c) : r(c.r), g(c.g), b(c.b), count(c.count) {}
 	bool operator<(const Color& c) const {
 		return (r + (g << 8) + (b << 16)) < (c.r + (c.g << 8) + (c.b << 16));
 	}
+	void addAvg(Vec3b c,int count)
+	{
+		r= (r*(count-1) +c[0])/count;
+		g= (g*(count-1) +c[0])/count;
+		b= (b*(count-1) +c[0])/count;
+	}
+	
+};
+
+bool close(Color A,Color B,int thresh)
+{
+	//std::cout<<"[[[[[[["<<(abs(A.r-B.r)+abs(A.g-B.g)+abs(A.b-B.b))<<"    "<<thresh<<std::endl;
+	/*if((abs(A.r-B.r)+abs(A.g-B.g)+abs(A.b-B.b)) < thresh)
+		return true;
+	return false;*/
+	if(abs(A.r-B.r)<thresh && abs(A.g-B.g)<thresh && abs(A.b-B.b)<thresh)
+		return true;
+	return false;
+}
+
+struct Quantum{
+	int index;
+	Color avg;
+	//long long count;
+	std::vector<Color > quants;
+	Quantum() : avg(), index(0) {}
+	Quantum(int i,Color c) : index(i), avg(c) {quants.push_back(c);}
+	//~Quantum();
+	
 };
 
 
@@ -35,7 +66,7 @@ bool sortFunc(const pair<Color, int> &p1, const pair<Color, int> &p2) {
 	return p1.second > p2.second;
 }
 
-void colorReduce(cv::Mat& image, int div =140)
+void colorReduce(cv::Mat& image, int div =32)
 {
 	int nl = image.rows;                    // number of lines
 	int nc = image.cols * image.channels(); // number of elements per line
@@ -53,13 +84,30 @@ void colorReduce(cv::Mat& image, int div =140)
 	}
 }
 
+bool inThreshold(Vec3b a,Vec3b b,int thresh)
+{
+	if(a[0] +thresh<b[0] || a[0]-thresh>b[0] )
+		return 0;
+	if(a[1] +thresh<b[1] || a[1]-thresh>b[1] )
+		return 0;
+	if(a[1] +thresh<b[1] || a[1]-thresh>b[1] )
+		return 0;
+	return 1;
+	//cout<<"in function: ("<<a[0]<<","<<a[1]<<","<<a[2]<<")\n";
+	/*if(a[0]==b[0] && a[1]==b[1] && a[2]==b[2])
+		return true;
+	return false;*/
+}
 
 
 int main(){
 	int d=25;
 	Mat img1 = imread("./pics/1.png", CV_LOAD_IMAGE_COLOR);
-	Mat img2 = img1;
+	cv::imshow("actual", img1);
+	Mat img2;
+	img1.copyTo(img2);
 	colorReduce(img2);
+
 	cv::imshow("colorReduce", img2);
 
 
@@ -110,150 +158,215 @@ int main(){
 			output[i].first.b << endl;
 	}
 
-	/*Mat temp = Mat(img.rows,img.cols, CV_8UC3);
+
+
+
+
+
+	//*******Calculating average of clusters corresponding to original image
+	vector<pair<Color, Color> > backtrack;
+	int count;
 	for(int i=0;i<output.size();i++)
 	{
-		//int i=5;
-		temp=Scalar(output[i].first.r,output[i].first.g,output[i].first.b);
-		imshow("colours",temp);
-		waitKey(0);
-
-	}*/
-
-
-
-	imwrite("imgout.png",img);
-	//Mat temp = Mat(img.rows,img.cols, CV_8UC1);
-	//temp=Scalar(0);
-	int count=0;
-	for(int i=0;i<output.size();i++)
-	{
-		//int i=2;
-		Mat temp = Mat(img.rows,img.cols, CV_8UC3);
-		Vec3b comparator;//= Scalar(output[i].first.r,output[i].first.g,output[i].first.b);
-		comparator[0]=output[i].first.r;
-		comparator[1]=output[i].first.g;
-		comparator[2]=output[i].first.b;
+		Vec3b comparator=Vec3b(output[i].first.r,output[i].first.g,output[i].first.b);//= Scalar(output[i].first.r,output[i].first.g,output[i].first.b);
+		count=0;
+		Color OrigAvg(0,0,0);
 		cout <<"color : " << output[i].first.r << " " << output[i].first.g << " " <<
 			output[i].first.b << endl;
 		for(int j=0;j<img.rows;j++)
 		{
 			for(int k=0;k<img.cols;k++)
 			{
-				//::Color c(img.at<Vec3b>(i,j));
-				//cout<<"Comparator : ("<<comparator[0]<<","<<comparator[1]<<","<<comparator[2]<<")\n";
-				//cout<<"Img values : ("<<img.at<Vec3b>(i,j)<<")\n";
 				if(comparator == img.at<Vec3b>(j,k))
 				{
+
 					count++;
-					temp.at<Vec3b>(j,k)[0]=255;//img.at<Vec3b>(j,k);
-					temp.at<Vec3b>(j,k)[1]=255;
-					temp.at<Vec3b>(j,k)[2]=255;
+					OrigAvg.addAvg(img1.at<Vec3b>(j,k),count);
+
+					
 				}
 			}
 		}
-		imshow("temp", temp);
-		//imshow("temp img", img);
+		OrigAvg.count=count;
+		backtrack.push_back(make_pair(output[i].first,OrigAvg));
+		cout<<"OrigAvg : ("<<OrigAvg.r<<","<<OrigAvg.g<<","<<OrigAvg.b<<")\n";
+		cout<<"count="<<count<<"\n";
+	}
+
+	map<Color,Color> MapBacktrack;
+	for(int i=0;i<backtrack.size();i++){
+		MapBacktrack[backtrack[i].second]=backtrack[i].first;
+	}
+
+	//backtrack fist is quantised outputs and second is average of each cluster corresponding to the first
+
+	//waitKey(0);
+	//vector<Quantum> data;
+	//Quantum Qtemp;
+	vector<pair<Color,int> > data;
+	for(int i=0;i<backtrack.size();i++){
+		data.push_back(make_pair(backtrack[i].second,0));
+	}
+	int thresh =7,mark=1;
+	for(int i=0;i<data.size();i++){
+		if(data[i].second!=0) continue;
+		data[i].second=mark;
+		mark++;
+		for(int j=0;j<data.size();j++){
+			if(data[j].second!=0) continue;
+			if(close(data[i].first,data[j].first,thresh)){
+				data[j].second=mark;
+			}
+		}
+	}
+	cout<<"MARK: "<<mark<<"\n";
+	//data= Merge(data);
+
+	/*for(int l=0;l<data.size();l++)
+		{
+			//cout<<"i and l : "<<i<<" "<<l<<"\n";
+			//if(data[i].second==i)
+				cout<<data[l].second<<"		"<<data[l].first.r<<" "<<data[l].first.g<<" "<<data[l].first.b<<"\n";
+		}*/
+
+	for(int i=1;i<mark;i++)
+	{
+		for(int l=0;l<data.size();l++)
+		{
+			//cout<<"i and l : "<<i<<" "<<l<<"\n";
+			if(data[l].second==i)
+				cout<<i<<"		"<<data[l].first.r<<" "<<data[l].first.g<<" "<<data[l].first.b<<"\n";
+		}
+	}
+
+	cout<<"Showing image backtracked seperation merged***********************************\n";
+	//comparing backtrack and priginal and showing images from original with grouping of clusters
+	Mat temp = Mat(img1.rows,img1.cols, CV_8UC3);
+	for(int i=1;i<mark;i++)
+	{
+		temp=Scalar(0,0,0);
+		int threshold=10;
+		for(int l=0;l<data.size();l++)
+		{
+			
+			if(data[l].second==i)
+			{
+
+				//Vec3b comparator=Vec3b(data[l].first.r,data[l].first.g,data[l].first.b);//= Scalar(output[i].first.r,output[i].first.g,output[i].first.b);
+				Color comp=MapBacktrack[data[l].first];
+				Vec3b comparator=Vec3b(comp.r,comp.g,comp.b);
+				cout<<"Comparator : "<<comparator<<" "<<i<<"\n";
+				for(int j=0;j<img1.rows;j++)
+				{
+					for(int k=0;k<img1.cols;k++)
+					{
+						if(inThreshold(comparator,img.at<Vec3b>(j,k),threshold))
+						{
+							//temp.at<Vec3b>(j,k)=Vec3b(255,255,255);//
+							temp.at<Vec3b>(j,k)=img1.at<Vec3b>(j,k);
+						}
+						
+					}
+				}	
+			}
+
+		}
+		//int i=2;
 		
+		//count=0;
+		//cout<<"OrigAvg : ("<<backtrack[i].second.r<<","<<backtrack[i].second.g<<","<<backtrack[i].second.b<<")\n";
+		cout<<"Waiting \n";
+		waitKey(0);
+		imshow("temp ", temp);
+		imshow("img1",img1);
+		cout<<"count=[]"<<"\n";
+	}
+
+
+
+	// cout<<"Showing image backtracked seperation***********************************\n";
+	// //comparing backtrack and priginal and showing images from original
+	// temp = Mat(img1.rows,img1.cols, CV_8UC3);
+	// for(int i=1;i<backtrack.size();i++)
+	// {
+	// 	//int i=2;
+	// 	temp=Scalar(0,0,0);
+	// 	count=0;
+	// 	Vec3b comparator=Vec3b(backtrack[i].second.r,backtrack[i].second.g,backtrack[i].second.b);//= Scalar(output[i].first.r,output[i].first.g,output[i].first.b);
+	// 	int threshold=50;
+	// 	cout<<"OrigAvg : ("<<backtrack[i].second.r<<","<<backtrack[i].second.g<<","<<backtrack[i].second.b<<")\n";
+	// 	for(int j=0;j<img1.rows;j++)
+	// 	{
+	// 		for(int k=0;k<img1.cols;k++)
+	// 		{
+	// 			if(inThreshold(comparator,img1.at<Vec3b>(j,k),threshold))
+	// 			{
+	// 				//cout<<inThreshold(comparator,img1.at<Vec3b>(i,j),threshold)<<endl;
+	// 				//cout<<inThreshold(comparator,img1.at<Vec3b>(i,j),threshold)<<endl;
+	// 				count++;
+	// 				temp.at<Vec3b>(j,k)[0]=255;
+	// 				temp.at<Vec3b>(j,k)[1]=255;
+	// 				temp.at<Vec3b>(j,k)[2]=255;
+	// 			}
+	// 			//else
+	// 				//cout<<"Values : "<<comparator<<","<<img.at<Vec3b>(j,k)<<","<<inThreshold(comparator,img1.at<Vec3b>(i,j),threshold)<<endl;
+					
+	// 		}
+	// 	}
+	// 	imshow("temp", temp);
+	// 	imshow("img1",img1);
+	// 	cout<<"count="<<count<<"\n";
+	// 	waitKey(0);
+		
+		
+
+
+	// }
+
+	cout<<"Showing image backtracked seperation over quantized values***********************************\n";
+	//comparing output[] and showing corresponding pixels in original image
+	 temp = Mat(img1.rows,img1.cols, CV_8UC3);
+	for(int i=1;i<backtrack.size();i++)
+	{
+		//int i=2;
+		temp=Scalar(0,0,0);
+		count=0;
+		Vec3b comparator=Vec3b(output[i].first.r,output[i].first.g,output[i].first.b);//= Scalar(output[i].first.r,output[i].first.g,output[i].first.b);
+		int threshold=0;
+		cout <<"color : " << output[i].first.r << " " << output[i].first.g << " " <<output[i].first.b << endl;
+		//cout<<"Comparator : "<<comparator<<" "<<i<<"\n";
+		for(int j=0;j<img.rows;j++)
+		{
+			for(int k=0;k<img.cols;k++)
+			{
+				if(inThreshold(comparator,img.at<Vec3b>(j,k),threshold))
+				{
+					count++;
+					/*temp.at<Vec3b>(j,k)[0]=255;
+					temp.at<Vec3b>(j,k)[1]=255;
+					temp.at<Vec3b>(j,k)[2]=255;*/
+					temp.at<Vec3b>(j,k)=img1.at<Vec3b>(j,k);
+				}
+				//else
+					//cout<<"Values : "<<comparator<<","<<img.at<Vec3b>(j,k)<<","<<inThreshold(comparator,img1.at<Vec3b>(i,j),threshold)<<endl;
+					
+			}
+		}
+		imshow("temp quantized", temp);
+		imshow("img1",img1);
 		cout<<"count="<<count<<"\n";
 		waitKey(0);
-		//temp = Scalar(0);
-		count=0;
+		
+		
 
 
 	}
 	
 
 
-	std::vector<cv::Mat> imgRGB;
-	cv::split(img, imgRGB);
-	int k = 4;
-	int n = img.rows *img.cols;
-	cv::Mat img3xN(n, 3, CV_8U);
-	for (int i = 0; i != 3; ++i)
-		imgRGB[i].reshape(1, n).copyTo(img3xN.col(i));
-	img3xN.convertTo(img3xN, CV_32F);
-	cv::Mat bestLables;
-	cv::kmeans(img3xN, k, bestLables, cv::TermCriteria(), 10, cv::KMEANS_RANDOM_CENTERS);
-	bestLables = bestLables.reshape(0, img.rows);
-	cv::convertScaleAbs(bestLables, bestLables, int(255 / k));
-	cv::imshow("result", bestLables);
+	
 
-	colorReduce(bestLables);
-	cv::imshow("result reduced", bestLables);
-
-
-
-
-	map<Color, int> histogram2;
-	for (int i = 0; i < bestLables.rows; i++) {
-		for (int j = 0; j < bestLables.cols; j++) {
-			::Color c(bestLables.at<Vec3b>(i,j));
-			if (histogram2.find(c) == histogram2.end())
-				histogram2[c] = 0;
-			histogram2[c]++;
-		}
-	}
-
-	cout << "map elements = " << histogram2.size() << endl;
-	vector<pair<Color, int> > output2;
-	for (map<Color, int>::reverse_iterator it = histogram2.rbegin(); it != histogram2.rend(); it++) {
-		output2.push_back(make_pair(it->first, it->second));
-	}
-
-	sort(output2.begin(), output2.end(), sortFunc);
-	for (int i = 0; i < 20 && i<output2.size(); i++) {
-		cout << "count2 = " << output2[i].second << "color2 : " << output2[i].first.r << " " << output2[i].first.g << " " <<
-			output2[i].first.b << endl;
-	}
-
-
-	/*//Mat temp = Mat(bestLables.rows,bestLables.cols, CV_8UC1);
-	temp=Scalar(0);
-	int count=0;
-	for(int i=2;i<output.size();i++)
-	{
-		//int i=2;
-		Mat temp = Mat(bestLables.rows,img.cols, CV_8UC1);
-		Vec3b comparator;//= Scalar(output[i].first.r,output[i].first.g,output[i].first.b);
-		comparator[0]=output[i].first.r;
-		comparator[1]=output[i].first.g;
-		comparator[2]=output[i].first.b;
-		cout <<"color : " << output[i].first.r << " " << output[i].first.g << " " <<
-			output[i].first.b << endl;
-		for(int j=0;j<img.rows;j++)
-		{
-			for(int k=0;k<img.cols;k++)
-			{
-				//::Color c(img.at<Vec3b>(i,j));
-				//cout<<"Comparator : ("<<comparator[0]<<","<<comparator[1]<<","<<comparator[2]<<")\n";
-				//cout<<"Img values : ("<<img.at<Vec3b>(i,j)<<")\n";
-				if(comparator == img.at<Vec3b>(j,k))
-				{
-					count++;
-					temp.at<Vec3b>(j,k)=255;
-				}
-			}
-		}
-		imshow("temp", temp);
-		imshow("temp img", img);
-		
-		cout<<"count="<<count<<"\n";
-		waitKey(0);
-		//temp = Scalar(0);
-		count=0;
-
-
-	}
-*/
-
-	/*for(int i=0;i<bestLables.rows;i++)
-	{
-		for(int j=0;j<bestLables.cols;j++)
-		{
-			cout<<bestLables.at<Vec3b>(i,j)<<"\n";
-		}
-	}*/
+	
 	cv::waitKey();
 
 
